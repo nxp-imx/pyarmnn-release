610869f4193b72af0e1068a1bad6a02261b8545c
Pavel Macenauer <pavel.macenauer@nxp.com>
2020-02-25 18:36:10 +0100

    AIR-2326 Updated the repository for PyArmNN 19.08 release: * Removed source package as not supported * Added documentation and updated the README.md file * Added examples * Regenerated the whl package with latest source code
    Signed-off-by: Pavel Macenauer <pavel.macenauer@nxp.com>

--- README.md
+++ README.md
@@ -2,28 +2,96 @@
 
 PyArmNN is a python extension for [Arm NN SDK](https://developer.arm.com/ip-products/processors/machine-learning/arm-nn).
 PyArmNN provides interface similar to Arm NN C++ Api.
+Before you proceed with the project setup, you will need to checkout and build a corresponding Arm NN version.
 
 PyArmNN is built around public headers from the armnn/include folder of Arm NN. PyArmNN does not implement any computation kernels itself, all operations are
-delegated to the Arm NN library. There is a few things which are reimplemented with Python in mind though.
+delegated to the Arm NN library. 
 
 The following diagram shows the conceptual architecture of this library:
 ![PyArmNN](./images/pyarmnn.png)
 
-This project contains prebuilt wheel and source packages to be installed using pip. Distributions are currently supported only for Python3.x versions.
-
 # PyArmNN installation
 
-PyArmNN is distributed as a source package or a binary package (wheel). Wheel package for ArmNN 19.08 is the only distribution, which is currently tested.
+PyArmNN is currently distributed only as a whl package (or also called binary package).
 
-Install dependencies:
-* Numpy (1.18.1)
+Binary package is platform dependent and the name of the package will indicate the platform it was built for. It also depends on the version of Python, e.g.:
 
-Runtime dependencies:
-* ArmNN libraries (19.08 or 19.11)
-* Protocol Buffers (3.5.1)
+* Arm NN 19.08 package for Python3.7/Linux Aarch 64 bit machine: pyarmnn-19.8.0-cp37-cp37m-linux_aarch64.whl
 
+The binary package requires the Arm NN library to be present on the target/build machine.
 
-To install use pip, e.g.:
+PyArmNN also depends on Numpy python library. It will be automatically downloaded and installed alongside PyArmNN. If your machine does not have access to Python pip repository you might need to install Numpy in advance by following public instructions: https://scipy.org/install.html
+
+## Installing from wheel
+
+Make sure that Arm NN binaries and Arm NN dependencies are installed and can be found in one of the system default library locations. You can check default locations by executing the following command:
+```bash
+$ gcc --print-search-dirs
+```
+Install PyArmNN from binary by pointing to the wheel file:
 ```bash
 $ pip3 install /path/to/pyarmnn-19.8.0-cp37-cp37m-linux_aarch64.whl
 ```
+
+# PyArmNN API overview
+
+#### Getting started
+The easiest way to begin using PyArmNN is by using the Parsers. We will demonstrate how to use them below:
+
+Create a parser object and load your model file.
+```python
+import pyarmnn as ann
+import imageio
+
+# ONNX, Caffe and TF parsers also exist.
+parser = ann.ITfLiteParser()  
+network = parser.CreateNetworkFromBinaryFile('./model.tflite')
+```
+
+Get the input binding information by using the name of the input layer.
+```python
+input_binding_info = parser.GetNetworkInputBindingInfo(0, 'model/input')
+
+# Create a runtime object that will perform inference.
+options = ann.CreationOptions()
+runtime = ann.IRuntime(options)
+```
+Choose preferred backends for execution and optimize the network.
+```python
+# Backend choices earlier in the list have higher preference.
+preferredBackends = [ann.BackendId('CpuAcc'), ann.BackendId('CpuRef')]
+opt_network, messages = ann.Optimize(network, preferredBackends, runtime.GetDeviceSpec(), ann.OptimizerOptions())
+
+# Load the optimized network into the runtime.
+net_id, _ = runtime.LoadNetwork(opt_network)
+```
+Make workload tensors using input and output binding information.
+```python
+# Load an image and create an inputTensor for inference.
+img = imageio.imread('./image.png')
+input_tensors = ann.make_input_tensors([input_binding_info], [img])
+
+# Get output binding information for an output layer by using the layer name.
+output_binding_info = parser.GetNetworkOutputBindingInfo(0, 'model/output')
+output_tensors = ann.make_output_tensors([outputs_binding_info])
+```
+
+Perform inference and get the results back into a numpy array.
+```python
+runtime.EnqueueWorkload(0, input_tensors, output_tensors)
+
+results = ann.workload_tensors_to_ndarray(output_tensors)
+print(results)
+```
+
+#### Running examples
+
+For a more complete Arm NN experience, there is a couple of examples located in the examples folder, which require requests, PIL and maybe some other python modules. You may install those using pip.
+
+To run these examples you may simply execute them using the python interpreter. There are no arguments and the resources are downloaded by the scripts:
+
+```bash
+$ python3 /path/to/examples/tflite_mobilenetv1_quantized.py
+```
+
+*example_utils.py* is a file containg common functions for the rest of the scripts and ot does not execute anything on its own. The rest of the scripts are the examples.
---
+++ examples/example_utils.py
@@ -0,0 +1,175 @@
+# Copyright 2020 NXP
+# SPDX-License-Identifier: MIT
+
+from PIL import Image
+import pyarmnn as ann
+import numpy as np
+import os
+import requests
+from urllib.parse import urlparse
+
+
+def create_tflite_network(model_file: str, backends: list = ['CpuAcc', 'CpuRef']):
+    """Creates a network from an onnx model file.
+
+    Args:
+        model_file (str): Path of the model file.
+        backends (list): List of backends to use when running inference.
+
+    Returns:
+        net_id: Network ID.
+        graph_id: Graph ID.
+        parser: TF Lite parser instance.
+        runtime: Runtime object instance.
+    """
+    options = ann.CreationOptions()
+    runtime = ann.IRuntime(options)
+    parser = ann.ITfLiteParser()
+
+    network = parser.CreateNetworkFromBinaryFile(model_file)
+
+    preferred_backends = []
+    for b in backends:
+        preferred_backends.append(ann.BackendId(b))
+
+    opt_network, _ = ann.Optimize(network, preferred_backends, runtime.GetDeviceSpec(), ann.OptimizerOptions())
+    net_id, _ = runtime.LoadNetwork(opt_network)
+    graph_id = parser.GetSubgraphCount() - 1
+
+    return net_id, graph_id, parser, runtime
+
+
+def create_onnx_network(model_file: str, backends: list = ['CpuAcc', 'CpuRef']):
+    """Creates a network from a tflite model file.
+
+    Args:
+        model_file (str): Path of the model file.
+        backends (list): List of backends to use when running inference.
+
+    Returns:
+        net_id: Network ID.
+        parser: ONNX parser instance.
+        runtime: Runtime object instance.
+    """
+    options = ann.CreationOptions()
+    runtime = ann.IRuntime(options)
+    parser = ann.IOnnxParser()
+
+    network = parser.CreateNetworkFromBinaryFile(model_file)
+
+    preferred_backends = []
+    for b in backends:
+        preferred_backends.append(ann.BackendId(b))
+
+    opt_network, _ = ann.Optimize(network, preferred_backends, runtime.GetDeviceSpec(), ann.OptimizerOptions())
+    net_id, _ = runtime.LoadNetwork(opt_network)
+
+    return net_id, parser, runtime
+
+
+def preprocess_default(img: Image, width: int, height: int, data_type, scale: float, mean: list, stddev: list):
+    """Default preprocessing image function.
+
+    Args:
+        img (PIL.Image): PIL.Image object instance.
+        width (int): Width to resize to.
+        height (int): Height to resize to.
+        data_type: Data Type to cast the image to.
+        scale (float): Scaling value.
+        mean (list): RGB mean offset.
+        stddev (list): RGB standard deviation.
+
+    Returns:
+        np.array: Resized and preprocessed image.
+    """
+    img = img.resize((width, height), Image.BILINEAR)
+    img = img.convert('RGB')
+    img = np.array(img)
+    img = np.reshape(img, (-1,3)) # reshape to [RGB][RGB]...
+    img = ((img / scale) - mean) / stddev
+    img = img.flatten().astype(data_type)
+    return img
+
+
+def load_images(image_files: list, input_width: int, input_height: int, data_type=np.uint8,
+                scale: float = 1., mean: list = [0., 0., 0.], stddev: list = [1., 1., 1.], preprocess_fn = preprocess_default):
+    """Loads images, resizes and performs any additional preprocessing to run inference.
+
+    Args:
+        img (list): List of PIL.Image object instances.
+        input_width (int): Width to resize to.
+        input_height (int): Height to resize to.
+        data_type: Data Type to cast the image to.
+        scale (float): Scaling value.
+        mean (list): RGB mean offset.
+        stddev (list): RGB standard deviation.
+        preprocess_fn: Preprocessing function.
+
+    Returns:
+        np.array: Resized and preprocessed images.
+    """
+    images = []
+    for i in image_files:
+        img = Image.open(i)
+        img = preprocess_fn(img, input_width, input_height, data_type, scale, mean, stddev)
+        images.append(img)
+    return images
+
+
+def load_labels(label_file: str):
+    """Loads a labels file containing a label per line.
+
+    Args:
+        label_file (str): Labels file path.
+
+    Returns:
+        list: List of labels read from a file.
+    """
+    with open(label_file, 'r') as f:
+        labels = [ l.rstrip() for l in f ]
+        return labels
+    return None
+
+
+def print_top_n(N: int, results: list, labels: list, prob: list):
+    """Prints TOP-N results
+
+    Args:
+        N (int): Result count to print.
+        results (list): Top prediction indices.
+        labels (list): A list of labels for every class.
+        prob (list): A list of probabilities for every class.
+
+    Returns:
+        None
+    """
+    assert(len(results) >= 1 and len(results) == len(labels) == len(prob))
+    for i in range(min(len(results), N)):
+        print("class={0} ; value={1}".format(labels[results[i]], prob[results[i]]))
+
+
+def download_file(url: str, force: bool=False, filename: str=None):
+    """Downloads a file.
+
+    Args:
+        url (str): File url.
+        force (bool): Forces to download the file even if it exists.
+        filename (str): Renames the file when set.
+
+    Returns:
+        str: Path to the downloaded file.
+    """
+    if filename is None: # extract filename from url when None
+        filename = urlparse(url)
+        filename = os.path.basename(filename.path)
+
+    print("Downloading '{0}' from '{1}' ...".format(filename, url))
+    if not os.path.exists(filename) or force is True:
+        r = requests.get(url)
+        with open(filename, 'wb') as f:
+            f.write(r.content)
+        print("Finished.")
+    else:
+        print("File already exists.")
+
+    return filename
